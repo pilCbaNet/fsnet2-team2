1c2a25b1d9fb64b80e5c0958e6d8408d4cc2c383
Marco  Iannuzzi <92125086+Marcoiannuzzi@users.noreply.github.com>
2022-10-07 08:14:24 -0300

    Login y registration Page

--- criptoWallet/src/app/app-routing.module.ts
+++ criptoWallet/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component:LoginComponent},
+  { path: 'registration', component:RegistrationComponent},
+  { path: 'transactions', component: TransactionComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- criptoWallet/src/app/app.module.ts
+++ criptoWallet/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HomeComponent } from './components/home/home.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
