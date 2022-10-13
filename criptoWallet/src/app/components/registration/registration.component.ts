4745b5253654eeef2867db87e33524cfbce7168b
Marco <marcoiannuzzi@live.com.ar>
2022-10-13 20:14:43 -0300

    validacion de registro

--- criptoWallet/src/app/components/registration/registration.component.ts
+++ criptoWallet/src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  regForm:FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.regForm = this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(6)]],
+      password2:['',[Validators.required, Validators.minLength(6)]],
+      address:['',[Validators.required]],
+      city:['',[Validators.required]],
+      terms:['',[Validators.requiredTrue]]
+    })
+   }
 
   ngOnInit(): void {
   }
