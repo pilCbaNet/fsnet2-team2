d0a1d0dd92a73a1af276f9540444bfaae2b2bae1
Nicolas Macias <anicolasmacias@gmail.com>
2022-10-19 18:56:26 -0300

    quienes-somos
    Quienes somos 1.0

--- criptoWallet/src/app/components/quienes-somos/quienes-somos.component.ts
+++ criptoWallet/src/app/components/quienes-somos/quienes-somos.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from 'src/app/services/team.service';
 
 @Component({
   selector: 'app-quienes-somos',
   templateUrl: './quienes-somos.component.html',
-  styleUrls: ['./quienes-somos.component.css']
+  styleUrls: ['./quienes-somos.component.css'],
 })
 export class QuienesSomosComponent implements OnInit {
+  team: any;
 
-  constructor() { }
+  constructor(private miServicio: TeamService) {}
 
   ngOnInit(): void {
+    this.miServicio.obtenerDatosTeam().subscribe((data) => {
+      this.team = data;
+    });
   }
-
 }
