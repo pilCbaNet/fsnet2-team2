ceb2ad2b1cf958df6356cfdccf3128484558dfdd
Marco <marcoiannuzzi@live.com.ar>
2022-11-24 20:34:03 -0300

    implementacion de DoCheck en navBar

--- criptoWallet/src/app/components/nav-bar/nav-bar.component.ts
+++ criptoWallet/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, DoCheck{
 
   token:any;
 
-  constructor() { }
+  constructor() {}
+
+  ngDoCheck(): void {
+    this.token = sessionStorage.getItem('token');
+  }
 
   ngOnInit(): void {
-    this.token = sessionStorage.getItem('token')
+    
   }
 
   logout(){
