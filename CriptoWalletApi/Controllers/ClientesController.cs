a46e8804a49c4c51b79fcffd891d7324b07e0660
Marco  Iannuzzi <92125086+Marcoiannuzzi@users.noreply.github.com>
2022-12-04 16:37:30 -0300

    endpoint de clientes generados

--- CriptoWalletApi/Controllers/ClientesController.cs
+++ CriptoWalletApi/Controllers/ClientesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CriptoWalletApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,35 +11,77 @@ namespace CriptoWalletApi.Controllers
     {
         // GET: api/<ClientesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Cliente> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Cliente> listaClientes;
+
+            try
+            {
+                using (var context = new BD_CRIPTOWALLETContext())
+                {
+                    listaClientes = context.Clientes.ToList();
+                }
+             
+                return listaClientes;
+            }
+            catch (Exception)
+            {
+                return Enumerable.Empty<Cliente>();
+            }           
         }
 
         // GET api/<ClientesController>/5
         [Route("GetById")]
         [HttpGet()]
-        public string Get(int id)
+        public Cliente Get(int id)
         {
-            return "value";
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                Cliente? clienteSelect = context.Clientes.FirstOrDefault(cl => cl.IdCliente == id);
+                return clienteSelect;
+            }
         }
 
         // POST api/<ClientesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Cliente cliente)
         {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                context.Clientes.Add(cliente);
+                context.SaveChanges();
+            }
         }
 
         // PUT api/<ClientesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Cliente cliente)
         {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                var clienteSelect = context.Clientes.FirstOrDefault(cl=> cl.IdCliente == cliente.IdCliente);
+                clienteSelect = cliente;
+                context.SaveChanges();
+            }
         }
 
         // DELETE api/<ClientesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(Cliente cliente)
         {
+            try
+            {
+                using (var context = new BD_CRIPTOWALLETContext())
+                {
+                    Cliente? clienteAEliminar = context.Clientes.FirstOrDefault(cl => cl.IdCliente == cliente.IdCliente);
+                    context.Clientes.Remove(clienteAEliminar);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
