50ea7c6ce86d79a01d9b2898f193ddc35f74238f
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2022-12-12 21:55:39 -0300

    cuentadto

---
+++ CriptoWalletApi/DTO/CuentaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace CriptoWalletApi.DTO
+{
+    public class CuentaDTO
+    {
+        public int IdCliente { get; set; }
+        public int Cbu { get; set; }
+        public string Alias { get; set; }
+        public int Monto { get; set; }
+        public int NumeroDeCuenta { get; set; }
+        public bool Estado { get; set; }
+    }
+}
