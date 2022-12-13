48726ffdb9c193e8e96921ab111b1743aa26fd0d
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2022-12-12 21:06:57 -0300

    api cuentas bancarias

--- CriptoWalletApi/Controllers/CuentasController.cs
+++ CriptoWalletApi/Controllers/CuentasController.cs
@@ -4,12 +4,95 @@ using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
+
+
+
+
+
+/*
+
+       
+
+        // POST api/<CuentasController>
+        [HttpPost]
+        public void Post([FromBody] CuentaDTO cuentaDTO)
+        {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                context.CuentasBancarias.Add(new CuentasBancaria
+                {
+                    IdCliente = cuentaDTO.IdCliente,
+                    Cbu=cuentaDTO.Cbu,
+                    Alias=cuentaDTO.Alias,
+                    Monto=cuentaDTO.Monto,
+                    NumeroDeCuenta=cuentaDTO.NumeroDeCuenta,
+                    Estado = cuentaDTO.Estado
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        // PUT api/<CuentasController>/5
+        [HttpPut("{numero}")]
+        public void Put([FromBody] CuentaDTO nuevaCuenta)
+        {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                var cuenta = context.CuentasBancarias.FirstOrDefault(cb => cb.NumeroDeCuenta == nuevaCuenta.NumeroDeCuenta);
+
+                if (cuenta != null)
+                { 
+                    cuenta.Monto = nuevaCuenta.Monto;
+                    context.SaveChanges();
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        // DELETE api/<CuentasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                var cuenta = context.CuentasBancarias.FirstOrDefault(cb => cb.IdCuenta == id);
+                if (cuenta != null)
+                {
+                    cuenta.Estado = false;
+                }
+                else
+                {
+
+                }
+                
+            }
+        }
+    }
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
+
 namespace CriptoWalletApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CuentasController : ControllerBase
     {
+
         // GET: api/<CuentasController>
         [HttpGet]
         public IEnumerable<CuentasBancaria> Get()
@@ -22,7 +105,7 @@ namespace CriptoWalletApi.Controllers
 
             return listaCuentas;
         }
-      
+       
 
         // GET api/<CuentasController>/5
         [Route("Id")]
@@ -31,18 +114,35 @@ namespace CriptoWalletApi.Controllers
         {
                 using (var context = new BD_CRIPTOWALLETContext())
                 {
-                    CuentasBancaria? cuentaBancariaSelect = context.CuentasBancarias.FirstOrDefault(cb => cb.IdCuenta == id);
+                    var cuentaBancariaSelect = context.CuentasBancarias.FirstOrDefault(cb => cb.IdCuenta == id);
                     return cuentaBancariaSelect;
                 }
 
         }
 
-
+        /*
+          [Route("byClient")]
+        [HttpPost]
+        public IEnumerable<CuentasBancaria> GetbyClient([FromBody]ClienteDTO cliente)
+        {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                var cuenta = context.CuentasBancarias.Where(cb => cb.IdCliente == cliente.IdCliente).ToList();
+                return cuenta;
+            }
+        }
+         */
 
         // POST api/<CuentasController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("byClient")]
+        [HttpPost()]
+        public IEnumerable<CuentasBancaria> GetbyClient([FromBody] ClienteDTO cliente)
         {
+            using (var context = new BD_CRIPTOWALLETContext())
+            {
+                var cuenta = context.CuentasBancarias.Where(cb => cb.IdCliente == cliente.IdCliente).ToList();
+                return cuenta;
+            }
         }
 
         
